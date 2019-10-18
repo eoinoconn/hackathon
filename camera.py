e2b0f1cc6a6b03043d8c6c0523f8efdcfbc0dce8
eoinoconn@gmail.com <eoinoconn@gmail.com>
2019-10-18 07:23:43 +0100

    camera functionality

--- camera.py
+++ camera.py
@@ -1,9 +1,37 @@
 
+"""Camera module for emote project
+"""
+import io
+import time
+
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+import cv2 # debug only
+import numpy as np
+
 class Camera():
+    """Camera object for processing images captured by pi and pi camera
+    """
 
-    def __init__():
-        pass
+    def __init__(self):
+        self.camera = PiCamera()
+        
 
-    def get_image():
+    def get_image(self):
+        """ Method to capture and return image
 
-        yield img
+        return:
+            numpy array: Image array
+        """
+        self.rawCapture = PiRGBArray(self.camera)
+        time.sleep(0.1)
+        self.camera.capture(self.rawCapture, format='bgr')
+        image = self.rawCapture.array
+        
+        return np.array(image)
+
+if __name__ == "__main__":
+    camera = Camera()
+    image = camera.get_image()
+    cv2.imshow("Figure", image)
+    cv2.waitKey(0)
