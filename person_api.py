166d386a334b6441639f508108f4f257cac95c30
Rafal Lasocki <rlasocki@jaguarlandrover.com>
2019-10-18 14:06:09 +0100

    almost done

--- person_api.py
+++ person_api.py
@@ -1,6 +1,4 @@
 
-import time
-
 class PersonAPI(object):
     SAD = 0
     HAPPY = 1
@@ -9,19 +7,33 @@ class PersonAPI(object):
     Presence = None
     Emotions = None
     def __init__(self, present_=None, emotions_=None):
+        print("Hello from PERSON api")
         self.Presence = present_
         self.Emotions = emotions_
 
     def calculate_emotions(self):
         if self.Emotions <= self.THRESHOLD:
             self.STATE = self.SAD
+            print("Not happy: Setting STATE to SAD")
         elif self.Emotions > self.THRESHOLD:
             self.STATE = self.HAPPY
+            print("Not sad: Setting STATE to HAPPY")
 
     def detection(self):
         if self.Presence > 0:
+            print("Face detected! Calculating Emotions")
             self.calculate_emotions()
         else:
-            print("No Detection")
+            print("No Detection... Are you there ?")
 
         return self.STATE
+
+
+
+def main():
+    print("Instantiate person object")
+    person = PersonAPI(0,0)
+    print(person.detection())
+
+if __name__ == '__main__':
+    main()
